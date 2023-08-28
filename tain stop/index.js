e00fc1e441c952c410766bd79cbc91bfc21df907
alvinronald123 <alvinronald123@gmail.com>
2023-08-28 11:54:50 -0700

    first commit

---
+++ tain stop/index.js
@@ -0,0 +1,18 @@
+//document.getElementById("count-el").innerText = 5
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+
+let count = 0
+
+function increment (){
+count = count + 1
+countEl.textContent = count 
+}
+function save(){
+let countStr = count + " -"
+
+saveEl.textContent += countStr 
+countEl.textContent = 0
+count = 0
+
+}
